35a553c03df0f6f3a3d49ddb6f7827ab5b47ec43
philwoz <61164019+philwoz@users.noreply.github.com>
2021-12-17 19:54:25 +0000

    added base template + navbar

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from main import views
 
 urlpatterns = [
-    path('home/', views.homepage, name='home')
+    path('home/', views.homepage, name='home'),
+    path('items/', views.itemspage, name='items')
 
 ]
